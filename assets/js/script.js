9c0a043965b3b904e09775b6d15fd687c73e9d88
Landfish744 <>
2023-11-16 21:02:11 +0000

    Fix h2 and p not showing on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ const playerScore = document.getElementById('player-score');
 const computerScore = document.getElementById('computer-score');
 const playerImage = document.getElementById('player-image');
 const computerImage = document.getElementById('computer-image');
-const messages = document.getElementById('messages');
 const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 
